bf3b20ec8b88957e20ccdf9c96727d99b8726750
rogersillito <rogersillito@gmail.com>
2015-06-14 11:41:01 +0100

    half finished promises work

---
+++ koans/deferred/src/deferred.js
@@ -0,0 +1,34 @@
+var SAMURAIPRINCIPLE = {};
+
+SAMURAIPRINCIPLE.Deferred = function() {
+    var doneCallbacks = [], isResolved, args, failedCallback;
+
+    this.done = function(f) {
+        doneCallbacks.push(f);
+        if (isResolved) {
+           this.resolve.apply(undefined, args); 
+        }
+        return this;
+    };
+
+    this.resolve = function() {
+        for (var i in doneCallbacks) {
+            doneCallbacks[i].apply(undefined, arguments);
+        }
+        args = arguments;
+        isResolved = true;
+        return this;
+    };
+
+    this.failed = function(f) {
+        failedCallback = f;
+        return this;
+    };
+
+    this.reject = function(argument) {
+        if (failedCallback) {
+            failedCallback(argument);
+        }
+        return this;
+    };
+};
